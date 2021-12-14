32f96d317584f1b7910f597be1ba196aec3aa10e
Joe Farrish <joseph_a_f@hotmail.com>
2021-12-14 05:40:30 -0700

    updated documentation

--- ZI_OOD/zorpion.py
+++ ZI_OOD/zorpion.py
@@ -1,4 +1,7 @@
 
+from types import prepare_class
+
+
 class Zorpion:
     def __init__(self):
         #set Zorpion
@@ -8,4 +11,8 @@ class Zorpion:
         self.zMv = 1 #The amount of capsules the zorpion can move per move can be 1 or 2. 
         self.zCompelled = False
         self.zrpPath = [] # [x, y]  setZPath.  
-        
+    
+    # Manage Logical Movement of the Zorpion
+    def zorpMv():
+        pass
+    
